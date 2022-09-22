f877b5faefc745346850fdf898b40e987e6e7acb
Lucas-ASouza <lucasalves-x@hotmail.com>
2022-09-22 09:22:35 -0300

    user edit interface added

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,6 +6,17 @@ class UserController {
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit()
+        this.onEditCancel();
+
+    }
+
+    onEditCancel(){
+
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e=>{
+
+            this.showPanelCreate();
+
+        });
 
     }
 
@@ -21,6 +32,11 @@ class UserController {
             // desabilita o botão submit enquanto processa a informação
 
             let values = this.getValues();
+
+
+            if (!values) return false;
+            //caso formulário tenha sido enviado incompleto ou de forma incorreta, não continuar com o código dessem metódo
+
 
             this.getPhoto().then(
                  (content)=>{
@@ -133,6 +149,8 @@ class UserController {
 
         let tr = document.createElement('tr');
 
+        tr.dataset.user = JSON.stringify(dataUser);
+
         tr.innerHTML = `
                 <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
@@ -140,13 +158,54 @@ class UserController {
                 <td>${(dataUser.admin)? 'Sim': 'Não'}</td>
                 <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                    <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
         `;
 
+        tr.querySelector(".btn-edit").addEventListener("click", e=>{
+
+            console.log(JSON.parse(tr.dataset.user));
+            this.showPanelUpdate();
+
+        });
+
         this.tableEl.appendChild(tr);
+
+        this.updateCount();
     }
 
+    showPanelCreate(){
+
+        document.querySelector("#box-user-create").style.display = "block";
+        document.querySelector("#box-user-update").style.display = "none";
+
+    }
+
+    showPanelUpdate(){
+
+        document.querySelector("#box-user-create").style.display = "none";
+        document.querySelector("#box-user-update").style.display = "block";
+
+    }
+
+    updateCount(){
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr => {
+            // '...' = SPREAD. Transforma coleção em array, só assim podemos usar forEach
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+
+            if (user._admin) numberAdmin++;
+
+        })
+        
+        
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
+    }
 
 }
